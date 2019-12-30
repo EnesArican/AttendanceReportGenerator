e0191e9b6ffa755d337f8e781a0703973d6f47e4
enesarican <enesarican@hotmail.com>
2019-12-30 11:54:03 +0000

    tidying up

--- Main.ps1
+++ Main.ps1
@@ -3,24 +3,23 @@ Import-Module .\Scripts\DataManager.psm1
 Import-Module .\Scripts\ExcelUtils.psm1
 Import-Module .\Scripts\HeadersManager.psm1
 
-#testing commit
+Write-Progress -Activity "Formatting" -Status "0% Complete - Opening Document" -PercentComplete 0
+
+
 $xlOpenXMLWorkbook = 51
 
 # load into Excel
-$excel = New-Object -ComObject Excel.Application 
-$excel.DisplayAlerts = $false
-$excel.Workbooks.Open("C:\Temp\daily_report.csv").SaveAs("C:\Temp\daily_report.xlsx",$xlOpenXMLWorkbook)
-$excel.Quit()
+$Path = "C:\Temp\daily_report.xlsx"
+$Excel = New-Object -ComObject Excel.Application 
+$Excel.DisplayAlerts = $false
+$Excel.Workbooks.Open("C:\Temp\daily_report.csv").SaveAs($Path, $xlOpenXMLWorkbook)
+#$excel.Quit()
 
 
-#Write-Progress -Activity "Formatting" -Status "0% Complete - Opening Excel Document" -PercentComplete 0
-
-# Get excel doc
-$path = "C:\Temp\daily_report.xlsx"
-$Excel = New-Object -Com Excel.Application
-$Workbook =  $Excel.Workbooks.Open($path, 0, $false) 
-$WorkSheet = $Workbook.worksheets.Item(1)
-$WorkSheet.activate()
+#$Excel = New-Object -Com Excel.Application
+$Workbook =  $Excel.Workbooks.Open($Path, 0, $false) 
+$Worksheet = $Workbook.worksheets.Item(1)
+$Worksheet.activate()
 
 
 Write-Progress -Activity "Formatting" -Status "10% Complete - Getting Records" -PercentComplete 10 
@@ -40,15 +39,14 @@ $Workbook.worksheets.add() | Out-Null
 $WorkSheet = $Workbook.worksheets.Item(1)
 $WorkSheet.activate()
 
+# Insert data
 Set-IhvanNames -worksheet $WorkSheet -nameArray $NameArray
 Set-DatesAndRecords -worksheet $WorkSheet -attendanceHash $AttendanceHash
 
-Write-Progress -Activity "Formatting" -Status "30% Complete - Making new Worksheet" -PercentComplete 30 
-
-# Format data added
+# Format data
 Format-NewWorksheet -worksheet $WorkSheet
 
-# Add  headers
+# Insert  headers
 Set-WorksheetHeaders -worksheet $WorkSheet
 
 # Format headers
